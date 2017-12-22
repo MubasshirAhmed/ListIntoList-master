cd410f7ed1e932c0c1b9d6f316889db53c159932
MubasshirAhmed <MubasshirAhmed26@Gmail.com>
2017-12-23 00:26:58 +0600

    Initioal commit

---
+++ ListIntoList/Assets/Scripts/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Person
+{
+
+    public int personId;
+    public string name;
+
+    public List<Phone> PhoneTypeAndNumber;
+}
